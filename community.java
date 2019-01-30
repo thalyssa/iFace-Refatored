927950af3d34070b65edcc45bb5697ca78f5eddc
Yami Thalyssa <thalyssa2.0@hotmail.com>
2019-01-30 11:28:05 -0300

    Update community.java

--- community.java
+++ community.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
-public class community {
+public class community extends messages{
 
     private int id;
     private ArrayList<user> members = new ArrayList<user>();
-    private ArrayList<String> msgs = new ArrayList<String>();
-    private moderator owner; //Dono da comunidade
+    private ArrayList<String> messages = new ArrayList<String>();
+    private user owner; //Dono da comunidade
     private String name;
     private String description;
 
@@ -13,11 +13,11 @@ public class community {
         return id;
     }
 
-    public moderator getOwner() {
+    public user getOwner() {
         return owner;
     }
 
-    public void setOwner(moderator owner) {
+    public void setOwner(user owner) {
         this.owner = owner;
     }
 
@@ -37,13 +37,6 @@ public class community {
         this.description = description;
     }
 
-    public community(int id, moderator owner, String name, String description){
-        this.id = id;
-        this.owner = owner;
-        this.name = name;
-        this.description = description;
-    }
-
     public void addMember(user newMember){
         members.add(newMember);
     }
@@ -53,5 +46,34 @@ public class community {
         for(int i=0;i<members.size();i++) {
             System.out.println(members.get(i).getUsername());
         }//Fim do for
+    }
+
+    public boolean isMember(user member){
+        for(int i=0;i<members.size();i++){
+            if(members.get(i).equals(member)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void recieveMessage(String msg){
+        messages.add(msg);
+    }
+
+    @Override
+    public void getMessages() {
+            System.out.println("--CAIXA DE MENSAGENS - COMUNIDADE:" + this.getName() + "--");
+            for(int i=0;i<messages.size();i++) {
+                System.out.println(messages.get(i) + "\n");
+            }
+    }
+
+    public community(int id, user owner, String name, String description){
+        this.id = id;
+        this.owner = owner;
+        this.name = name;
+        this.description = description;
     }
 }
